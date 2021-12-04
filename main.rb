18082d6422147a01614bccb5f645e855dbd32e3f
takuya nagasawa <tsukasayanagawa474@gmail.com>
2021-12-04 18:07:36 +0900

    Itemクラス作成

--- main.rb
+++ main.rb
@@ -1,9 +1,10 @@
 require "pry"
+require_relative "item"
 
 SHOP_NAME = "ラーメン屋　りゅーたろう"
 MONEY = 3000
 
-items = Item.impotr(path: "items.csv")
+items = Item.import(path: "items.csv")
 shop = Shop.new(name: SHOP_NAME, items: items)
 user = User.new(money: MONEY)
 
